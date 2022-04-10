baff7cb1eaaecfb2e59de36a5811516169460022
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-04-10 17:03:21 +0300

    8 adding ability to stop time

--- components/TimeControls.jsx
+++ components/TimeControls.jsx
@@ -7,11 +7,18 @@ const TimeControls = ({clockTime, handleMultiplierChange, isNavOpen}) => {
     const [currentTime, setCurrentTime] = useState(null);
 
     const handleTimeChange = (e) => {
-        // calculate exponentially rising multiplier
-        let multiplier = Math.round(Math.exp((Math.log(10000)/100) * Math.abs(e.target.value)));
-        if (e.target.value < 0) {
-            multiplier = multiplier * -1;
+        let multiplier = 0;
+        if (Math.abs(e.target.value) < 2) {
+            // set time as stopped
+            multiplier = 0;
+        } else {
+            // calculate exponentially rising multiplier
+            multiplier = Math.round(Math.exp((Math.log(10000)/100) * Math.abs(e.target.value)));
+            if (e.target.value < 0) {
+                multiplier = multiplier * -1;
+            }
         }
+
         setTimeMultiplier(multiplier);
         handleMultiplierChange(multiplier);
     };
@@ -32,7 +39,13 @@ const TimeControls = ({clockTime, handleMultiplierChange, isNavOpen}) => {
                 </p>
                 <p className={styles["time-text"]}>
                     Time flow multiplier:
-                    <span className={styles["time-text-bold"]}> { timeMultiplier }x</span>
+                    <span className={styles["time-text-bold"]}>
+                        {
+                            timeMultiplier === 0 ?
+                            " 0x (Stopped)" :
+                            ` ${timeMultiplier}x`
+                        }
+                    </span>
                 </p>
             </div>
             <input
