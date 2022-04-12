c74209ca9d75aead938bd79a1ed2ddc7ea709fb5
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-04-12 11:16:39 +0300

    10 adding favicon and metadata to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,24 @@
 import Link from "next/link";
+import Head from 'next/head';
+import Layout from "components/Layout";
 import styles from "styles/404.module.css";
 
 const NotFound = () => {
   return (
-    <div className={styles["not-found"]}>
-      <h1 className={styles["not-found__heading"]}>
-        This page does not exist.
-      </h1>
-      <Link href="/">
-        <a className={styles["not-found__link"]}>Go to home page</a>
-      </Link>
-    </div>
+    <Layout>
+      <Head>
+        <title>Deimantas Butėnas - Satellite Tracker - Page not found</title>
+      </Head>
+      
+      <div className={styles["not-found"]}>
+        <h1 className={styles["not-found__heading"]}>
+          This page does not exist.
+        </h1>
+        <Link href="/">
+          <a className={styles["not-found__link"]}>Go to home page</a>
+        </Link>
+      </div>
+    </Layout>
   );
 };
 
